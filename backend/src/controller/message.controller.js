5c18f10eb4c13cf15c872c0dd3f6fd9a2a6a5754
hridayroy19 <hrhridoyroy503@gmail.com>
2025-05-21 22:59:33 +0600

    create getmessage

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -1,3 +1,21 @@
-export const getUserSidbar = async (req, res) => {
+import Message from "../models/message.model";
 
-}
+export const getUserSidbar = async (req, res) => {};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const message = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("error in get message",error.message)
+    res.status(500).json({error:"internal server error"})
+  }
+};
--- backend/src/routers/message.route.js
+++ backend/src/routers/message.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { protecRoute } from '../middleware/auth.medilware';
-import { getUserSidbar } from '../controller/message.controller.js';
+import { getMessages, getUserSidbar } from '../controller/message.controller.js';
 
 const router = express.Router();
 
-router.post('/user',protecRoute,getUserSidbar);
+router.get('/user',protecRoute,getUserSidbar);
+
+router.get('/:id',protecRoute,getMessages);
+
+router.post('/send/:id',protecRoute,sendMessages);
 
 
 export default router;
